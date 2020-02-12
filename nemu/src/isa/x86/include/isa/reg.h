e3fbe56744da326babb224c4885fb982322a76bb
tracer-ics2019 <tracer@njuics.org>
2020-02-13 00:20:01 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-28-generic #30~18.04.1-Ubuntu SMP Fri Jan 17 06:14:09 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:20:01 up 16:13,  1 user,  load average: 0.57, 0.62, 0.42 e6cd844ee6cf0224b865cbc941026a93538e2d22

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -15,7 +15,17 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
  * For more details about the register encoding scheme, see i386 manual.
  */
-
+/* x86的通用寄存器有eax、ebx、ecx、edx、edi、esi。这些寄存器在大多数指令中是可以
+ * 任意选用的，比如movl指令可以把一个立即数传送到eax中，也可传送到ebx中。
+ * 但也有一些指令规定只能用其中某个寄存器做某种用途，
+ * 例如除法指令idivl要求被除数在eax寄存器中，edx寄存器必须是0，而除数可以在任意寄存器中，
+ * 计算结果的商数保存在eax寄存器中（覆盖原来的被除数），余数保存在edx寄存器中。
+ * 也就是说，通用寄存器对于某些特殊指令来说也不是通用的。
+ * x86的特殊寄存器有ebp、esp、eip、eflags。eip是程序计数器，
+ * eflags保存着计算过程中产生的标志位，
+ * 其中包括进位标志、溢出标志、零标志和负数标志，
+ * 在intel的手册中这几个标志位分别称为CF、OF、ZF、SF。
+ * ebp和esp用于维护函数调用的栈帧*/
 typedef struct {
   union {
     union {
