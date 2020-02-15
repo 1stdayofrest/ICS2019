babdf311b1e27e3ea5a592b414a644fbca524b6b
tracer-ics2019 <tracer@njuics.org>
2020-02-16 02:11:20 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-28-generic #30~18.04.1-Ubuntu SMP Fri Jan 17 06:14:09 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:11:20 up  7:58,  1 user,  load average: 1.33, 1.14, 1.13 d2c70c94c0c407e91a58a2a518ee83eb60dfe77

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -14,7 +14,7 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-  printf("hello windows remote");
+//  printf("hello windows remote");
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
