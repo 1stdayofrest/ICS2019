47b442b1f945e6429db727b806712796a1b6c467
tracer-ics2019 <tracer@njuics.org>
2020-02-15 20:51:49 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-28-generic #30~18.04.1-Ubuntu SMP Fri Jan 17 06:14:09 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:51:49 up  2:39,  2 users,  load average: 0.09, 0.18, 0.18 298c577ce1d8409e23379cae25ede0105680efa4

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,5 +1,7 @@
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+
+#include <stdio.h>
 /*
  * 我们已经知道, NEMU是一个用来执行客户程序的程序,
  * 但客户程序一开始并不存在于客户计算机中.
@@ -12,7 +14,7 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-
+  printf("hello windows remote");
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
