e0f2e70443491285b7b7ee37535e8bafd805bce6
tracer-ics2019 <tracer@njuics.org>
2020-02-22 18:45:58 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:45:58 up 11:59,  1 user,  load average: 0.85, 0.48, 0.33 d8da368b26b42f92e8b2fdf46b88545950e0189

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -1,23 +1,24 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
-
+/* 把通用寄存器和PC与从QEMU中读出的寄存器的值进行比较. 若对比结果一致, 函数返回true;
+ * 如果发现值不一样, 函数返回false, 框架代码会自动停止客户程序的运行*/
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->eax != cpu.eax || ref_r->ecx != cpu.ecx || ref_r->edx != cpu.edx ||
      ref_r->ebx != cpu.ebx || ref_r->esp != cpu.esp || ref_r->ebp != cpu.ebp ||
      ref_r->esi != cpu.esi || ref_r->edi != cpu.edi || ref_r->pc != cpu.pc) {
-    printf("qemus eax:0x%08x, mine eax:0x%08x @pc:0x%08x\n", ref_r->eax, cpu.eax, cpu.pc);
-    printf("qemus ecx:0x%08x, mine ecx:0x%08x @pc:0x%08x\n", ref_r->ecx, cpu.ecx, cpu.pc);
-    printf("qemus edx:0x%08x, mine edx:0x%08x @pc:0x%08x\n", ref_r->edx, cpu.edx, cpu.pc);
-    printf("qemus ebx:0x%08x, mine ebx:0x%08x @pc:0x%08x\n", ref_r->ebx, cpu.ebx, cpu.pc);
-    printf("qemus esp:0x%08x, mine esp:0x%08x @pc:0x%08x\n", ref_r->esp, cpu.esp, cpu.pc);
-    printf("qemus ebp:0x%08x, mine ebp:0x%08x @pc:0x%08x\n", ref_r->ebp, cpu.ebp, cpu.pc);
-    printf("qemus esi:0x%08x, mine esi:0x%08x @pc:0x%08x\n", ref_r->esi, cpu.esi, cpu.pc);
-    printf("qemus edi:0x%08x, mine edi:0x%08x @pc:0x%08x\n", ref_r->edi, cpu.edi, cpu.pc);
-    printf("qemus pc :0x%08x, mine pc :0x%08x @pc:0x%08x\n", ref_r->pc, cpu.pc, cpu.pc);
-    return true;
+    printf("qemus eax:0x%08x, mine eax:0x%08x @pc:0x%08x\n", ref_r->eax, cpu.eax, pc);
+    printf("qemus ecx:0x%08x, mine ecx:0x%08x @pc:0x%08x\n", ref_r->ecx, cpu.ecx, pc);
+    printf("qemus edx:0x%08x, mine edx:0x%08x @pc:0x%08x\n", ref_r->edx, cpu.edx, pc);
+    printf("qemus ebx:0x%08x, mine ebx:0x%08x @pc:0x%08x\n", ref_r->ebx, cpu.ebx, pc);
+    printf("qemus esp:0x%08x, mine esp:0x%08x @pc:0x%08x\n", ref_r->esp, cpu.esp, pc);
+    printf("qemus ebp:0x%08x, mine ebp:0x%08x @pc:0x%08x\n", ref_r->ebp, cpu.ebp, pc);
+    printf("qemus esi:0x%08x, mine esi:0x%08x @pc:0x%08x\n", ref_r->esi, cpu.esi, pc);
+    printf("qemus edi:0x%08x, mine edi:0x%08x @pc:0x%08x\n", ref_r->edi, cpu.edi, pc);
+    printf("qemus pc :0x%08x, mine pc :0x%08x @pc:0x%08x\n", ref_r->pc, cpu.pc, pc);
+    return false;
   }
 
-  return false;
+  return true;
 }
 
 void isa_difftest_attach(void) {
