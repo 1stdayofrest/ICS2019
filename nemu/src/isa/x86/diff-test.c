a7056334c7d5f70a4593403a6b33021d1e4800a9
tracer-ics2019 <tracer@njuics.org>
2020-02-22 18:39:41 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:39:41 up 11:53,  1 user,  load average: 0.66, 0.44, 0.29 7508e800ed99d3c4421f2693e69c5de6be2c29da

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,6 +2,21 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  if(ref_r->eax != cpu.eax || ref_r->ecx != cpu.ecx || ref_r->edx != cpu.edx ||
+     ref_r->ebx != cpu.ebx || ref_r->esp != cpu.esp || ref_r->ebp != cpu.ebp ||
+     ref_r->esi != cpu.esi || ref_r->edi != cpu.edi || ref_r->pc != cpu.pc) {
+    printf("qemus eax:0x%08x, mine eax:0x%08x @pc:0x%08x\n", ref_r->eax, cpu.eax, cpu.pc);
+    printf("qemus ecx:0x%08x, mine ecx:0x%08x @pc:0x%08x\n", ref_r->ecx, cpu.ecx, cpu.pc);
+    printf("qemus edx:0x%08x, mine edx:0x%08x @pc:0x%08x\n", ref_r->edx, cpu.edx, cpu.pc);
+    printf("qemus ebx:0x%08x, mine ebx:0x%08x @pc:0x%08x\n", ref_r->ebx, cpu.ebx, cpu.pc);
+    printf("qemus esp:0x%08x, mine esp:0x%08x @pc:0x%08x\n", ref_r->esp, cpu.esp, cpu.pc);
+    printf("qemus ebp:0x%08x, mine ebp:0x%08x @pc:0x%08x\n", ref_r->ebp, cpu.ebp, cpu.pc);
+    printf("qemus esi:0x%08x, mine esi:0x%08x @pc:0x%08x\n", ref_r->esi, cpu.esi, cpu.pc);
+    printf("qemus edi:0x%08x, mine edi:0x%08x @pc:0x%08x\n", ref_r->edi, cpu.edi, cpu.pc);
+    printf("qemus pc :0x%08x, mine pc :0x%08x @pc:0x%08x\n", ref_r->pc, cpu.pc, cpu.pc);
+    return true;
+  }
+
   return false;
 }
 
