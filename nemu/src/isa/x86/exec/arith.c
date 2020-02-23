b1e8f68c570e04402ea467b6d9a45a143236a8d7
tracer-ics2019 <tracer@njuics.org>
2020-02-23 15:16:56 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:16:56 up 22:28,  1 user,  load average: 0.67, 0.24, 0.13 c093d971dd2c2c68c9a15c1a6ed2cec1f20cd15b

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -72,19 +72,51 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-  TODO();
+  //TODO
+  rtl_addi(&t2, &id_dest->val, 1);
+  rtl_setrelop(RELOP_LTU, &t3, &t2, &id_dest->val);
+  operand_write(id_dest, &t2);
 
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_set_CF(&t3);
+
+  rtl_xori(&t0, &id_dest->val, 1);
+  rtl_not(&t0, &t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
   print_asm_template1(inc);
 }
 
 make_EHelper(dec) {
-  TODO();
+  //TODO
+  rtl_subi(&t2, &id_dest->val, 1);
+  rtl_setrelop(RELOP_LTU, &t3, &id_dest->val, &t2);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_set_CF(&t3);
+
+  rtl_xori(&t0, &id_dest->val, 1);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(dec);
 }
 
 make_EHelper(neg) {
-  TODO();
+  //TODO
+  rtl_not(&t0, &id_dest->val);
+  rtl_addi(&t1, &t0, 1);
+  operand_write(id_dest, &t1);
+
+  rtl_li(&t2, id_dest->val != 0);
+  rtl_set_CF(&t2);
 
   print_asm_template1(neg);
 }
