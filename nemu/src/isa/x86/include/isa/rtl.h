5a718ec98bc5b671a7f1aef48f710b62f916844a
tracer-ics2019 <tracer@njuics.org>
2020-02-23 16:49:17 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:49:17 up 1 day, 0 min,  1 user,  load average: 0.05, 0.18, 0.28 6ddb56eccabbe9898be2fb9192bb721d5ee6286c

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -82,7 +82,7 @@ static inline void rtl_is_add_carry(rtlreg_t *dest, const rtlreg_t *res,
 //对eflags寄存器的set，get操作，简单赋值操作？？？
 #define make_rtl_setget_eflags(f)                                              \
   static inline void concat(rtl_set_, f)(const rtlreg_t *src) {                \
-    cpu.eflags.f = *src & 0x1;                                                       \
+    cpu.eflags.f = *src | 0x0;                                                       \
   }                                                                            \
   static inline void concat(rtl_get_, f)(rtlreg_t * dest) {                    \
     *dest = cpu.eflags.f;                                                      \
@@ -118,7 +118,7 @@ static inline void rtl_update_SF(const rtlreg_t *result, int width) {
   // TODO
   rtl_msb(&t0, result, width);
   //rtlreg_t is_sign = t0 != 0;
-  rtl_set_SF(&t0);
+  rtl_set_SF(&t0);//TODO:有问题
   //t0 = result[width * 8 - 1]; //找到符号位
   //rtl_set_SF(&t0);            //设置符号位
 }
