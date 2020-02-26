c450ca818f39766bcc7765d9e9acb0145d350b88
tracer-ics2019 <tracer@njuics.org>
2020-02-26 17:18:48 +0800

    >  compile 181220000 lizq Linux ubuntu 5.3.0-40-generic #32~18.04.1-Ubuntu SMP Mon Feb 3 14:05:59 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:18:48 up  3:00,  1 user,  load average: 0.32, 0.38, 0.36 c54c8801473ec72b72101ba2b21e65b64c90d2b3

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -13,52 +13,64 @@
  * 第一个默认为0，往后依次加一。
  * 这就是讲义中说的哪个需要更新eflags的操作。
  * 根据手册勘误来完成相关的操作！*/
-void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
+void rtl_setcc(rtlreg_t *dest, uint8_t subcode) {
   bool invert = subcode & 0x1;
   enum {
-    CC_O, CC_NO, CC_B,  CC_NB,
-    CC_E, CC_NE, CC_BE, CC_NBE,
-    CC_S, CC_NS, CC_P,  CC_NP,
-    CC_L, CC_NL, CC_LE, CC_NLE
+    CC_O,
+    CC_NO,
+    CC_B,
+    CC_NB,
+    CC_E,
+    CC_NE,
+    CC_BE,
+    CC_NBE,
+    CC_S,
+    CC_NS,
+    CC_P,
+    CC_NP,
+    CC_L,
+    CC_NL,
+    CC_LE,
+    CC_NLE
   };
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
-/*
-  switch (subcode & 0xe) {
-  case CC_O://0
-    rtl_get_OF(dest);
-    break;
-  case CC_B://2
-    rtl_get_CF(dest); //小于，通过CF来判断不够减
-    break;
-  case CC_E://4
-    rtl_get_ZF(dest);
-    break;
-  case CC_BE: { //6
-    rtl_get_CF(&t0);
-    rtl_get_ZF(&t1);
-    rtl_or(dest, &t0, &t1); //小于等于，CF和ZF至少一个要等于1才行
-  }break;
-  case CC_S: //8
-    rtl_get_SF(dest);
-    break;
-  case CC_L: { //12
-    rtl_get_SF(&t0);
-    rtl_get_OF(&t1);
-    rtl_xor(dest, &t1, &t0); //带符号数的小于，SF不能等于OF
-  }break;
-  case CC_LE: { //14
-    rtl_get_ZF(&t0);
-    rtl_get_SF(&t1);
-    rtl_get_OF(&t2);
-    rtl_xor(&t3, &t1, &t2);
-    rtl_or(dest, &t0, &t3); //带符号数的小于等于，ZF=1或者SF不等于OF
-  }break;
-    default: panic("should not reach here");
-    case CC_P: panic("n86 does not have PF");
-  }
-*/
+  /*
+    switch (subcode & 0xe) {
+    case CC_O://0
+      rtl_get_OF(dest);
+      break;
+    case CC_B://2
+      rtl_get_CF(dest); //小于，通过CF来判断不够减
+      break;
+    case CC_E://4
+      rtl_get_ZF(dest);
+      break;
+    case CC_BE: { //6
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      rtl_or(dest, &t0, &t1); //小于等于，CF和ZF至少一个要等于1才行
+    }break;
+    case CC_S: //8
+      rtl_get_SF(dest);
+      break;
+    case CC_L: { //12
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(dest, &t1, &t0); //带符号数的小于，SF不能等于OF
+    }break;
+    case CC_LE: { //14
+      rtl_get_ZF(&t0);
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      rtl_xor(&t3, &t1, &t2);
+      rtl_or(dest, &t0, &t3); //带符号数的小于等于，ZF=1或者SF不等于OF
+    }break;
+      default: panic("should not reach here");
+      case CC_P: panic("n86 does not have PF");
+    }
+  */
   /*switch (subcode & 0xe) {
   case CC_O:  //0
     *dest = cpu.eflags.OF;
@@ -95,8 +107,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   case CC_BE: *dest = cpu.eflags.CF | cpu.eflags.ZF ? 1 : 0; break;
   case CC_S: *dest = cpu.eflags.SF ? 1 : 0; break;
   case CC_L: *dest = cpu.eflags.SF != cpu.eflags.OF ? 1 : 0; break;
-  //case CC_LE: *dest = cpu.eflags.SF != cpu.eflags.OF || cpu.eflags.ZF ? 1 : 0; break;
-  case CC_LE: //14 e
+  //case CC_LE: *dest = cpu.eflags.SF != cpu.eflags.OF || cpu.eflags.ZF ? 1 : 0;
+  break; case CC_LE: //14 e
     //dest = ((cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF));
     rtl_get_ZF(&t0);
     rtl_get_SF(&t1);
@@ -108,33 +120,39 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   case CC_P: panic("n86 does not have PF");
   }*/
   switch (subcode & 0xe) {
-  case CC_O:  //0
+  case CC_O: // 0
     *dest = cpu.eflags.OF;
     break;
-  case CC_B:  //2
+  case CC_B: // 2
     *dest = cpu.eflags.CF;
     break;
-  case CC_E:  //4
-    //printf("i am here\n");
-    //printf("zf = %d\n", cpu.eflags.ZF);
+  case CC_E: // 4
+    // printf("i am here\n");
+    // printf("zf = %d\n", cpu.eflags.ZF);
     *dest = cpu.eflags.ZF;
     break;
-  case CC_BE: //6
-    //printf("switch = %x\n", subcode&0xe);
+  case CC_BE: // 6
+    // printf("switch = %x\n", subcode&0xe);
     *dest = ((cpu.eflags.CF) || (cpu.eflags.ZF));
-    //printf("dest = %d\n", *dest);
+    // printf("dest = %d\n", *dest);
     break;
-  case CC_S:  //8
+  case CC_S: // 8
     *dest = cpu.eflags.SF;
     break;
-  case CC_L:  //12 c
+  case CC_L: // 12 c
     *dest = (cpu.eflags.SF != cpu.eflags.OF);
     break;
-  case CC_LE: //14 e 将ZF和SF是否不等于OF的结果取或，然后赋值给dest
-    *dest = ((cpu.eflags.ZF != cpu.eflags.OF) || (cpu.eflags.SF != cpu.eflags.OF));
+  case CC_LE: // 14 e 将ZF和SF是否不等于OF的结果取或，然后赋值给dest
+    rtl_get_ZF(&t0);
+    rtl_get_SF(&t1);
+    rtl_get_OF(&t2);
+    // t3 = (t1 != t2 ? 1 : 0);
+    rtl_li(dest, (t0 || (t1 != t2)));
     break;
-  default: panic("should not reach here");
-  case CC_P: panic("n86 does not have PF");
+  default:
+    panic("should not reach here");
+  case CC_P:
+    panic("n86 does not have PF");
   }
   if (invert) {
     rtl_xori(dest, dest, 0x1);
